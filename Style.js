84755815867812de82a3262205f516cf0a76b2d6
SamDevPhp <132905586+SamDevPhp@users.noreply.github.com>
2023-05-09 07:03:37 +0200

    Sam

---
+++ Style.js
@@ -0,0 +1,10 @@
+let Sam = document.querySelector('.Sam')
+let hero = document.querySelector('.hero')
+function TimCook(Apple){
+    Sam.src = Apple;
+    console.log(Apple)
+    // alert("hi")
+}
+function color(watch){
+    hero.style.background = watch;
+}
